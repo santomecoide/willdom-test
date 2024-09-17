29894ac42c8f68ee12131bc4b4470f214f602d21
Santiago <smecoide@gmail.com>
2024-09-17 05:33:10 -0500

    add readmin to root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import Handlebars from "handlebars";
+import Handlebars from 'handlebars';
 import fs from 'fs';
 
 import DbClient from './db-client'
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import routes from './app.route'
+import fs from 'fs';
+import { marked } from 'marked';
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use('/api/v1', routes);
+
+app.get('/', function (req, res) {
+  const readme = fs.readFileSync('readme.md', 'utf-8')
+  res.send(marked(readme.toString()));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
